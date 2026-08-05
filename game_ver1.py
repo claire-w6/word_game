aeaad835017882a591157aadb9c7bd9e7405f4b0
claire-w6 <clairewang1616@gmail.com>
2026-08-04 23:29:12 -0500

    feat: Add Score counter

--- game_ver1.py
+++ game_ver1.py
@@ -2,6 +2,7 @@
 import json
 
 USED_WORDS = set()
+SCORE = 0
 LAST_LETTER = ''
 
 def load_words():
@@ -10,47 +11,53 @@ def load_words():
     keys = set(data.keys())
     return keys
 
+def checkWord(wordBank: set, word: str) -> bool:
+    first_letter = word[0]
+    global LAST_LETTER
+    global SCORE
+
+    if LAST_LETTER == '':
+        LAST_LETTER = word[-1]
+    else:
+        if LAST_LETTER != first_letter:
+            raise ValueError("The first letter does not match the previous word's last letter. " 
+                + f"Try again. Last letter is {LAST_LETTER}.")
+    
+    # First word
+    if not USED_WORDS:
+        USED_WORDS.add(word)
+        SCORE += 1
+        return
+
+    if word not in USED_WORDS:
+        if word in wordBank:
+            USED_WORDS.add(word)
+            SCORE += 1
+            LAST_LETTER = word[-1]
+        else:
+            raise ValueError(f"The word {word} does not exist.")
+    else:
+        raise ValueError(f"The word {word} has already been used. Think of another one.")
+
 def game():
     wordBank = load_words()
 
     while True:
         word = input("Word: ")
         if word == "Safe Word":
-            print("Safe word employed! Exiting game.")
+            print(f"Safe word employed... Exiting game. Game score: {SCORE}")
             break
-        
-        first_letter = word[0]
-        global LAST_LETTER
-
-        if LAST_LETTER == '':
-            LAST_LETTER = word[-1]
-        else:
-            if LAST_LETTER != first_letter:
-                print("The first letter does not match the previous word's last letter." 
-                    + f"Try again. Last letter is {LAST_LETTER}.")
-                continue
-
-        
-        
-        # First word
-        if not USED_WORDS:
-            USED_WORDS.add(word)
+        try:
+            checkWord(wordBank, word)
+            print(f"Current score: {SCORE}")
+        except ValueError as e:
+            print(e)
             continue
-
-        if word not in USED_WORDS:
-            if word in wordBank:
-                USED_WORDS.add(word)
-                LAST_LETTER = word[-1]
-            else:
-                print(f"The word {word} does not exist.")
-        else:
-            print(f"The word: {word} has already been used. Think of another one.")
+        
     return
 
 
 
 
 if __name__ == "__main__":
-    # user_name = input("Enter your name: ")
-    # print(f"Hello, {user_name}! Launching word game.")
     game()
