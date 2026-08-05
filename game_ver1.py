8566ab417a7dbc527cebd53abb31201f2bc5221c
claire-w6 <clairewang1616@gmail.com>
2026-08-04 23:58:09 -0500

    feat: Add scoreboard

--- game_ver1.py
+++ game_ver1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import json
 
+# ---- CONSTANTS/GLOBALS ----
 USED_WORDS = set()
 SCORE = 0
 LAST_LETTER = ''
+SCORE_BOARD = {1:0, 2:0, 3:0}
 
 def load_words():
     with open('words_dictionary.json') as word_file:
@@ -39,6 +41,32 @@ def checkWord(wordBank: set, word: str) -> bool:
     else:
         raise ValueError(f"The word {word} has already been used. Think of another one.")
 
+def checkScore():
+    global SCORE_BOARD
+    first = SCORE_BOARD[1]
+    second = SCORE_BOARD[2]
+    third = SCORE_BOARD[3]
+    if SCORE > first:
+        SCORE_BOARD[1] = SCORE
+        SCORE_BOARD[2] = first
+        SCORE_BOARD[3] = second
+        print("You made it onto the scoreboard!")
+    elif SCORE > second and SCORE < first:
+        SCORE_BOARD[2] = SCORE
+        SCORE_BOARD[3] = second
+        print("You made it onto the scoreboard!")
+    elif SCORE > third and SCORE < second:
+        SCORE_BOARD[3] = SCORE
+        print("You made it onto the scoreboard!")
+    print("--------------------------------------------")
+    print("-----------------SCOREBOARD-----------------")
+    print("--------------------------------------------")
+    print(f"1st: {SCORE_BOARD[1]}")
+    print(f"2nd: {SCORE_BOARD[2]}")
+    print(f"3rd: {SCORE_BOARD[3]}")
+    print("--------------------------------------------")
+
+
 def game():
     wordBank = load_words()
 
@@ -46,6 +74,7 @@ def game():
         word = input("Word: ")
         if word == "Safe Word":
             print(f"Safe word employed... Exiting game. Game score: {SCORE}")
+            checkScore()
             break
         try:
             checkWord(wordBank, word)
